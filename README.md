142331c1b431973b1aeb5667cf9fb59910cfabc1
Durvesh <durvesh099@gmail.com>
2022-03-17 13:57:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+docker exec -it react-flask-app_db_1 mongo /scripts/InitDB.js
---
+++ server/app.py
@@ -0,0 +1,38 @@
+import random
+from flask import Flask, request
+from pymongo import MongoClient
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'OK'
+
+
+@app.route('/quote')
+def quote():
+
+    app.logger.info(f"ENTERING: {request.path}")
+
+    client = MongoClient('db')
+    db = client.quotesdb
+    data = list(db.quotes.find())
+
+    if len(data) < 1:
+        return {
+            "id": 0,
+            "quote": "Remember to init the db with InitDB.js"
+        }
+
+    row = random.choice(data)
+    return {
+        "id": int(row['id']),
+        "quote": row['quote']
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
